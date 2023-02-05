20694989f71e5ddddbee7ed2ee22b7ad81543cfd
Matthew Wigglesworth <mpw@MPW-MBP.local>
2023-02-04 19:41:06 -0800

    nav to header on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,16 +12,10 @@ export default function RootLayout({
     <html lang='en'>
       <head />
       <body>
+        <Nav />
         <div className={styles.container}>
           <main className={styles.main}>{children}</main>
         </div>
-        {/* <video autoPlay muted loop id='bth-home-reel'>
-          <source
-            src='https://static.videezy.com/system/resources/previews/000/044/479/original/banana.mp4'
-            type='video/mp4'
-          />
-        </video> */}
-        <Nav />
       </body>
     </html>
   );
