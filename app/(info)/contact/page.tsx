fe72d9a5cdc20646efff06deac95741635d792d3
Matthew Wigglesworth <mpw@MPW-MBP.local>
2023-02-09 01:49:48 -0800

    swap links to contacts

--- app/(info)/contact/page.tsx
+++ app/(info)/contact/page.tsx
@@ -6,17 +6,11 @@ export default function Contact() {
       <h1>Contact Me:</h1>
       <hr />
       <p>
-        <b>
-          <a href='mailto: danielduvall22@gmail.com'>Email:</a>
-        </b>{' '}
-        danielduvall22@gmail.com
+        <b>Email:</b>{' '}
+        <a href='mailto: danielduvall22@gmail.com'>danielduvall22@gmail.com</a>
         <br />
-        <b>
-          <a href='tel:814-494-0811'>Phone:</a>
-        </b>{' '}
-        (+1) 814-494-0811
+        <b>Phone:</b> <a href='tel:814-494-0811'>(+1) 814-494-0811</a>
       </p>
-
       <ContactForm />
     </section>
   );
